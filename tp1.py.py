820d9c614e5e0cfe77accfff06d3d1d4df7ed39a
balberti <popotamalberti@gmail.com>
2020-10-21 12:18:28 +0200

    ajout volcano

--- tp1.py.py
+++ tp1.py.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 get_ipython().run_line_magic('matplotlib', 'nbagg')
@@ -12,7 +12,7 @@ import csv
 import pandas as pd
 
 
-# In[2]:
+# In[3]:
 
 
 fig, ax = plt.subplots()
@@ -28,7 +28,7 @@ ax.plot(x, np.full(len(x), 0.2),
 fig.show()
 
 
-# In[3]:
+# In[4]:
 
 
 tsv = pd.read_csv("/home/balberti/Documents/S5/protéo/tp1/tp-proteomics/data/TCL_wt1.tsv",delimiter="\t")
@@ -38,7 +38,7 @@ tsv.iloc[:, 3:7]=tsv.iloc[:, 3:7].astype(float)
 tsv
 
 
-# In[4]:
+# In[5]:
 
 
 log2_CAR=tsv["Log2 Corrected Abundance Ratio"]
@@ -49,21 +49,21 @@ plt.title("Histogramme de la variable Log2 Corrected Abundance Ratio")
 plt.hist(log2_CAR)
 
 
-# In[5]:
+# In[6]:
 
 
 mu=np.mean(log2_CAR)
 mu
 
 
-# In[6]:
+# In[7]:
 
 
 S_2=np.var(log2_CAR)*np.sqrt(len(log2_CAR)/(len(log2_CAR)+1))
 S_2
 
 
-# In[7]:
+# In[8]:
 
 
 fig, ax = plt.subplots()
@@ -79,7 +79,7 @@ ax.set_title("Histogramme de log2 corrected abundance ratio et sa densité de pr
 plt.show()
 
 
-# In[8]:
+# In[9]:
 
 
 log10_pval=tsv["-LOG10 Adj.P-val"]
@@ -100,14 +100,14 @@ plt.show()
 # seuil : Alpha = 1*10-3  -log10 de 0.0001 (a gauche) / mu+sigma =-0.17 (a droite) ou -0.28 (tout ce qui est a mu + 1 ecart type est sur abondant)
 # Donc en gros on prend que ce qui est dans le carré en haut à droite
 
-# In[9]:
+# In[10]:
 
 
 df_ORA=tsv.loc[(tsv['Log2 Corrected Abundance Ratio']>mu+np.sqrt(S_2)) & (tsv['-LOG10 Adj.P-val']>-np.log10(0.001))]
-df_ORA
+df_ORA["Accession"]
 
 
-# In[10]:
+# In[11]:
 
 
 from xml.etree.ElementTree import parse, dump
@@ -133,7 +133,7 @@ for iden in identifiant:
 
 # J'ai récupéré ces résultats et utilisé sublim text pour ne récupérer que les identifiants Go d'intérêts. C'est une opération manuelle mais je n'arrivais pas à le faire avec un code. Il était plus important pour moi de contourner le problème pour continuer le TP.
 
-# In[11]:
+# In[12]:
 
 
 go_id=open("data/Go_id.txt",'r')
@@ -143,6 +143,24 @@ for line in go_id:
 liste_seule=set(liste)
 print(len(liste))
 len(liste_seule)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
